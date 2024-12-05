5bacb9f8ea27b7932b24996375b63a573968a79d
silentsilas <me@silentsilas.com>
2024-12-04 23:02:44 -0500

    solve d03p2

--- lib/advent/year2024/day03.ex
+++ lib/advent/year2024/day03.ex
@@ -1,4 +1,5 @@
 defmodule Advent.Year2024.Day03 do
+  @type instruction() :: {integer(), {:do, boolean()} | {:mul, integer()}}
   def part1(args) do
     ~r/mul\((\d+),(\d+)\)/
     |> Regex.scan(args)
@@ -10,5 +11,55 @@ defmodule Advent.Year2024.Day03 do
 
   def part2(args) do
     args
+    |> find_all_instructions()
+    |> Enum.sort()
+    |> process_instructions()
+  end
+
+  @spec find_all_instructions(String.t()) :: [instruction()]
+  def find_all_instructions(input) do
+    muls = find_multiplications(input)
+    dos = find_dos(input)
+    donts = find_donts(input)
+    muls ++ dos ++ donts
+  end
+
+  @spec find_multiplications(String.t()) :: [instruction()]
+  def find_multiplications(input) do
+    Regex.scan(~r/mul\((\d+),(\d+)\)/, input, return: :index)
+    |> Enum.map(fn [{pos, len} | nums] ->
+      [num1, num2] =
+        Enum.map(nums, fn {pos, len} ->
+          String.to_integer(String.slice(input, pos, len))
+        end)
+
+      {pos, {:mul, num1 * num2}}
+    end)
+  end
+
+  @spec find_dos(String.t()) :: [instruction()]
+  def find_dos(input) do
+    Regex.scan(~r/do\(\)/, input, return: :index)
+    |> Enum.map(fn [{pos, _len}] -> {pos, {:do, true}} end)
+  end
+
+  @spec find_donts(String.t()) :: [instruction()]
+  def find_donts(input) do
+    Regex.scan(~r/don't\(\)/, input, return: :index)
+    |> Enum.map(fn [{pos, _len}] -> {pos, {:do, false}} end)
+  end
+
+  @spec process_instructions([instruction()]) :: integer()
+  def process_instructions(instructions) do
+    {_pos, {_enabled, sum}} =
+      Enum.reduce(instructions, {0, {true, 0}}, fn
+        {pos, {:do, enabled}}, {_last_pos, {_enabled, sum}} ->
+          {pos, {enabled, sum}}
+
+        {pos, {:mul, result}}, {_last_pos, {enabled, sum}} ->
+          {pos, {enabled, sum + if(enabled, do: result, else: 0)}}
+      end)
+
+    sum
   end
 end
--- test/advent/year2024/day03_test.exs
+++ test/advent/year2024/day03_test.exs
@@ -17,19 +17,67 @@ defmodule Advent.Year2024.Day03Test do
 
       assert result == 169_021_493
     end
+
+    test "solves part 2 with example data" do
+      input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      result = part2(input)
+
+      assert result == 48
+    end
+
+    test "solves part 2 with fixture data" do
+      input = File.read!("test/fixtures/day03p1_input.txt")
+      result = part2(input)
+
+      assert result == 111_762_583
+    end
   end
 
-  test "solves part 2 with example data" do
-    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-    result = part2(input)
+  describe "find_multiplications/1" do
+    test "finds multiplication instructions with positions" do
+      input = "mul(2,4)other_mul(3,5)"
+      result = find_multiplications(input)
 
-    assert result == 48
+      assert result == [{0, {:mul, 8}}, {14, {:mul, 15}}]
+    end
   end
 
-  test "solves part 2 with fixture data" do
-    input = File.read!("test/fixtures/day03p1_input.txt")
-    result = part2(input)
+  describe "find_dos/1" do
+    test "finds do instructions with positions" do
+      input = "something_do()_other_do()"
+      result = find_dos(input)
 
-    assert result == 0
+      assert result == [{10, {:do, true}}, {21, {:do, true}}]
+    end
+  end
+
+  describe "find_donts/1" do
+    test "finds don't instructions with positions" do
+      input = "something_don't()_other_don't()"
+      result = find_donts(input)
+
+      assert result == [{10, {:do, false}}, {24, {:do, false}}]
+    end
+  end
+
+  describe "process_instructions/1" do
+    test "processes instructions in order and returns sum" do
+      instructions = [
+        # enabled = true, sum = 8
+        {0, {:mul, 8}},
+        # enabled = false, sum = 8
+        {5, {:do, false}},
+        # enabled = false, sum = 8
+        {10, {:mul, 15}},
+        # enabled = true, sum = 8
+        {15, {:do, true}},
+        # enabled = true, sum = 18
+        {20, {:mul, 10}}
+      ]
+
+      result = process_instructions(instructions)
+
+      assert result == 18
+    end
   end
 end
